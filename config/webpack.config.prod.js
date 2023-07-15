8ed3c91a110b031ce65472b73a94344685a3dff3
Saeid Barzegar <saeidbarzegar@gmail.com>
2023-07-15 15:11:09 +0800

    minimizer and optimization config added

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -2,11 +2,39 @@ const { merge } = require("webpack-merge");
 const path = require('path')
 const commonConfig = require('./webpack.config.common');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // extracts css files in different style file on build
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 const devConfig = {
   mode: 'production',
   output: {
     filename: 'js/[name].[contenthash].js'
+  },
+  /**
+   * by default webpack has it's own optimization rules
+   * and by default will compress the output js file
+   * but when we adding optimization section it will use this configuration
+   * and we need to add separate rule to minify the JS files as well
+   * But in webpack 5 these we can add a config to tells webpack 
+   * to run default webpack rules as well as new rules
+   * The `...` line in first line of minimizer will do that
+   */
+  optimization: {
+    minimize: true,
+    minimizer: [
+      `...`, // to use default rules as well
+      // this CssMinimizerPlugin plugin used to minify the css file in production build
+      new CssMinimizerPlugin({ 
+        minimizerOptions: {
+          preset: [
+            "default", // default rule
+            {
+              // to remove comments in build file
+              discardComments: { removeAll: true },
+            },
+          ],
+        },
+      }),
+    ],
   },
   module: {
     rules: [
